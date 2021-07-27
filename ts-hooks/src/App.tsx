28ca091d53da3bfe4bd2be71415877423cc10436
NANGGI <mqsora@naver.com>
2021-07-27 13:38:45 +0900

    Add files via upload

---
+++ ts-hooks/src/App.tsx
@@ -0,0 +1,13 @@
+import UseStateComponent from './UseStateComponent';
+
+function App() {
+  return (
+    <div>
+      <h1>useEffct</h1>
+      <h1>useState</h1>
+      <UseStateComponent />
+    </div>
+  );
+}
+
+export default App;
